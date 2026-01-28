c0fb14808538ef67c82dbf2318627fce43a71405
Marcus-CSI <marcus.mabarrack@student.edu.au>
2026-01-29 01:04:30 +1030

    Test works for shuffle now

--- tests/decktest.cpp
+++ tests/decktest.cpp
@@ -11,7 +11,7 @@ int main(){
     for (int i = 1; i <= 52; i++){//made i = 1 for testing purposes
         std::cout << Udeck[i-1].get_gameValue() << " | ";
         Udeck[i-1].print_cardSuit();
-        //std::cout<< i << std::endl;
+        std::cout<< i << std::endl;
     }
 
     std::cout << "Deck A has | " << DeckA.get_no_of_cards() << " number of cards" << std::endl;
@@ -20,9 +20,11 @@ int main(){
     DeckA.shuffleDeck();
     std::cout<<"Shuffle Deck\n"<< "---------------------" << std::endl;
 
-    for (int i = 0; i <= 52; i++){//made i = 1 for testing purposes
-        std::cout << DeckA.get_deck()[i].get_gameValue() << " | ";
-        //DeckA.get_deck()[0].print_cardSuit(); needs to be fixed
+    for (int i = 1; i <= 52; i++){//made i = 1 for testing purposes
+        std::cout << DeckA.get_deck()[i-1].get_gameValue() << " | ";
+        Card Card3 = DeckA.get_deck()[i-1];
+        Card3.print_cardSuit(); //needs to be fixed
+        std::cout<< i << std::endl;
     }
     //---------------- Working on Shuffle Function-----------------------
 
